d0f3be57d42e2d545a67418c3f7e21d8451cf86f
Irving Orozco <irvingorozco@gmail.com>
2018-05-15 01:37:30 -0400

    Edicion en grid

---
+++ WebApplication2/Models/ErrorVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class ErrorVM
+    {
+        public string status { get; set; } = "error";
+
+        public string message { get; set; } 
+    }
+}
